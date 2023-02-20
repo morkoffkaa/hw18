6dcd35c45318641bf451aba8ce626e0aa9490b0b
Olena Kaliuzhna <116544261+morkoffkaa@users.noreply.github.com>
2023-02-20 17:29:22 +0200

    Update test.js

--- test.js
+++ test.js
@@ -6,31 +6,14 @@
 // sum(20) = 28
 
 
-// function adder(num1) {
-//     return function(num2) {
-//         return num1 + num2;
-//     }
-// }
-
-// let add3 = adder(3);
-// let add5 = adder(5);
-// let add8 = adder(8);
-
-// console.log(add3);
-// console.log(add3(5));
-// console.log(add8(20));
-
-
-
-function createSum () {
-    let total = 0;
-    return function sum(x) {
-        return total += x;
+function adder() {
+    let sum = 0;
+    return function(num1) {
+        return sum += (num1);
     }
 };
 
-
-const result = createSum();
+const result  = adder();
 
 
 console.log(result(3));
