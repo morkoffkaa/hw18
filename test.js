22bc125fb57e2d0bce1572405d74fad9b62a2650
Olena Kaliuzhna <116544261+morkoffkaa@users.noreply.github.com>
2023-02-20 15:48:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -6,16 +6,33 @@
 // sum(20) = 28
 
 
-function adder(num1) {
-    return function(num2) {
-        return num1 + num2;
+// function adder(num1) {
+//     return function(num2) {
+//         return num1 + num2;
+//     }
+// }
+
+// let add3 = adder(3);
+// let add5 = adder(5);
+// let add8 = adder(8);
+
+// console.log(add3);
+// console.log(add3(5));
+// console.log(add8(20));
+
+
+
+function createSum () {
+    let total = 0;
+    return function sum(x) {
+        return total += x;
     }
-}
+};
 
-let add3 = adder(3);
-let add5 = adder(5);
-let add8 = adder(8);
 
-console.log(add3);
-console.log(add3(5));
-console.log(add8(20));
+const result = createSum();
+
+
+console.log(result(3));
+console.log(result(5));
+console.log(result(20));
